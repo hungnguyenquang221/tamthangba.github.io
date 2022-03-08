0a259b76e19fd4ef7d19681e424a074d01ca275b
hungnguyenquang221 <95635825+hungnguyenquang221@users.noreply.github.com>
2022-03-08 17:24:53 +0700

    upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Mẹ Yêu';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 8-3';
+const titleCard = 'Tặng mẹ yêu';
+const contentCard = 'Chúc mẹ của con một ngày 8-3 tràn ngập niềm vui và những nụ cười, luôn xinh đẹp, mau hết bệnh. Luôn vui vẻ trong hôm nay và cả những ngày sau. Con yêu mẹ <3';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = '275148629_297718352300068_1245944862218033464_n.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
